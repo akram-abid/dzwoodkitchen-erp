77f3803d48636c20d51405e75e9b7460138e6961
Ibrahim Abderrahim Meriah <mer.r4him@gmail.com>
2026-08-01 00:35:51 +0100

    feat(auth): check cookies in the routes wrapper instead of Auth header + return JWT in the cookies & it's settings

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -3,7 +3,19 @@ import { login } from "@/app/services/userAuthService";
 
 export async function POST(req) {
   try {
-    return NextResponse.json(await login(await req.json()));
+    const loginData = await login(await req.json());
+
+    const resp = NextResponse.json({loginData}); 
+
+    resp.cookies.set("token", token, {
+        httpOnly:true, 
+        secure: false, // for the meantime
+        sameSite: "lax", 
+        path: "/",
+        maxAge: 60*60*24*7, // 7DAYS 
+    }); 
+
+    return resp; 
   } catch (err) {
     return NextResponse.json({ error: err.message }, { status: 401 });
   }
